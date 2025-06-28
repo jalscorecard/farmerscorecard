be139346543e593e4cfab1a3bfb1d027efd7af11
jalscorecard <scorecard@jalsmruti.org>
2025-06-28 14:52:59 +0530

    Update FarmerAgriFarm

--- frontend/src/pages/FarmerAgriForm.jsx
+++ frontend/src/pages/FarmerAgriForm.jsx
@@ -45,7 +45,7 @@ export default function FarmerAssessmentForm() {
 
     setLoading(true);
     try {
-      const res = await fetch("http://localhost:5000/api/submit", {
+      const res = await fetch("https://farmerbackend-kzaw.onrender.com/api/submit", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
